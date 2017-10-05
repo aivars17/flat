14c7531907e79d5cba57823c7d46e36340bbdeab
aivars17 <30680298+aivars17@users.noreply.github.com>
2017-10-05 10:38:21 +0300

    Initial commit

---
+++ index.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Flat</title>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css">
+	
+</head>
+<body>
+	<div class="container-fluid">
+		<div class="row">
+			<div class="col-5 text-center pirmas" id="t-1"><h1>Welcome to our web</h1></div>
+			<div class="col text-center pirmas penktas " id="t-2">This is our flat design. It looks great and does the job. Why not try to use it?</div>
+			<div class="col text-center pirmas penktas" id="t-3">Generated with SASS and Bootstrap. Now we know it!</div>
+		</div>
+		<div class="row">
+			<div class="col antras penktas text-center" id="t-2"><h2>My portfolio is HERE</h2></div>
+			<div class="col antras penktas text-center blog" id="t-1">Blog</div>
+			<div class="col-6  text-center tas" id="t-4">
+				<div class="row">
+					<div class="col-3 lang-1">Photo album:</div>
+					<div class="col-3 lang-2" id="t-2">Foto 1</div>
+				</div>
+				<div class="row">
+					<div class="col-3 lang-3" id="t-3">Foto 2</div>
+					<div class="col-3 lang-4" id="t-1">Foto 3</div>
+				</div>
+		</div>
+		</div>
+		<div class="row">
+			<div class="col trecias text-center" id="t-3"><i class="fa fa-facebook-official fa-3x" aria-hidden="true"></i></div>
+			<div class="col trecias text-center" id="t-1"><i class="fa fa-instagram fa-3x" aria-hidden="true"></i></div>
+			<div class="col trecias text-center" id="t-3"><i class="fa fa-envelope-o fa-3x" aria-hidden="true"></i></div>
+			<div class="col sestas text-center"></div>
+			<div class="col trecias text-center contact" id="t-1">Contacts</div>
+		</div>
+		<div class="row">
+			<div class="col text-center ketvirtas f-1" id="f-1">1</div>
+			<div class="col text-center f-2" id="f-2">2</div>
+			<div class="col text-center f-3" id="f-3">3</div>
+			<div class="col text-center f-4" id="f-4">4</div>
+			<div class="col text-center f-5" id="f-5">5</div>
+			<div class="col text-center f-6" id="f-6">6</div>
+			<div class="col text-center f-7" id="f-7">7</div>
+			<div class="col text-center f-8" id="f-8">8</div>
+			<div class="col text-center f-9" id="f-9">9</div>
+		</div>
+		<div class="row"><div class="col footer"></div></div>
+	</div>
+</body>
+</html>
